a3c7c46f22b6dedd62f1539a66ffc0f142e73190
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-04-16 22:42:30 +0200

    3 fixed

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -6,3 +6,9 @@ ALTER TABLE group_member ADD INDEX index_group_member_on_user_id(user_id);
 
 -- api.jsの394行目 allActive
 ALTER TABLE record ADD INDEX index_record_on_status_update_at(status, updated_at DESC);
+
+-- 407 allActive
+ALTER TABLE record_last_access ADD INDEX index_record_last_access_on_user_id_record_id(user_id, record_id);
+
+-- 262 tomeAccess
+ALTER TABLE record ADD INDEX index_record_on_category_id_application_group(category_id, application_group);
--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,7 +1,7 @@
 -- getRecord, 146行目
 ALTER TABLE group_member ADD INDEX index_group_member_on_user_id_is_primary(user_id, is_primary);
 -- tomeActive, 293行目
-ALTER TABLE record_comment ADD INDEX index_record_comment_on_linked_record_id(linked_record_id);
+ALTER TABLE record_comment ADD INDEX index_record_comment_on_linked_record_id(linked_record_id DESC);
 -- allActive, 481行目
 ALTER TABLE record ADD INDEX index_record_on_status(status);
 -- mineActive, 711行目
