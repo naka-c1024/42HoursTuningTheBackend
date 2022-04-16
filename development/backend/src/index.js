09b004ae0d593c897e9b4db0eeae44de79abc92d
tkanzaki <tkanzaki@student.42tokyo.jp>
2022-04-16 03:06:44 +0200

    helloworld削除counter add

--- development/backend/src/index.js
+++ development/backend/src/index.js
@@ -5,14 +5,11 @@ app.use(express.json({limit: '10mb'}))
 
 const api = require("./api");
 
-app.get('/api/hello', (req, res) => {
-  console.log('requested');
-  res.send({ response :'World!'})
-})
-
 app.post('/api/client/records', async (req, res, next) => {
   try {
+    console.time('counter');
     await api.postRecords(req, res);
+    console.time('counter');
   } catch(e) {
     console.log(e);
     next(new Error("Unexpect"));
