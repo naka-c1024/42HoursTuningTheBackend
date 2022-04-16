3b9e4aa11aec88607df21926b2fde22d063e0bcd
naka-c1024 <ny42800824@gmail.com>
2022-04-16 20:53:16 +0900

    fix: index.jsを元に戻した

--- development/backend/src/index.js
+++ development/backend/src/index.js
@@ -6,10 +6,8 @@ app.use(express.json({limit: '10mb'}))
 const api = require("./api");
 
 app.get('/api/hello', (req, res) => {
-  console.time('hello label');
   console.log('requested');
-  res.send({ response :'ynakashi'})
-  console.timeEnd('hello label');
+  res.send({ response :'world!'})
 })
 
 app.post('/api/client/records', async (req, res, next) => {
