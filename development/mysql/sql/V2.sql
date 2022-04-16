33b9a8bd32dd0c7b287a8bd917b8ff7782fe302c
naka-c1024 <ny42800824@gmail.com>
2022-04-16 22:08:56 +0900

    update: V2.sql comment追加

--- development/mysql/sql/V2.sql
+++ development/mysql/sql/V2.sql
@@ -1,3 +1,6 @@
-ALTER TABLE record ADD INDEX (status);
-ALTER TABLE record ADD INDEX (created_by, status);
-ALTER TABLE record_comment ADD INDEX (linked_record_id);
+-- tomeActive, 293行目
+ALTER TABLE record_comment ADD INDEX index_record_comment_on_linked_record_id(linked_record_id);
+-- allActive, 481行目
+ALTER TABLE record ADD INDEX index_record_on_status(status);
+-- mineActive, 711行目
+ALTER TABLE record ADD INDEX index_record_on_created_by_status(created_by, status);
